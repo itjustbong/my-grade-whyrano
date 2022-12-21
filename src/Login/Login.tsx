7a58953a9653513e98a8345c961ddc57a4ffe000
Jchan <gkswocks123@gmail.com>
2022-12-21 14:36:16 +0900

    Feat : Add SVG

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Input } from './Login.styled';
-import { ReactComponent as MainLogo } from '../../public/assets/main_logo.svg';
+import { ReactComponent as MainLogo } from '../assets/main_logo.svg';
 
 const Login = () => {
   return (
     <div>
       로그인 화면
+      <MainLogo />
       <Input />
     </div>
   );
