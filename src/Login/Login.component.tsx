6a955c5e88febd58f012be6d35d7fc6c46680ce8
Jchan <gkswocks123@gmail.com>
2022-12-21 17:04:19 +0900

    Feat : 리펙토링

--- src/Login/Login.component.tsx
+++ src/Login/Login.component.tsx
@@ -1,22 +1,31 @@
-import { Empty, InputID, InputPW, ButtonLogIn } from './Login.styled';
+import { Empty, Input, Button, Form } from './Login.styled';
+import { Typography } from 'antd';
 
 /* [Todo] 다시 리펙토링 */
 export function LoginBox(): JSX.Element {
   return (
     <>
-      <form
-        id="sign-in"
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
+      <Form>
         <InputID />
         <Empty height="1rem" />
         <InputPW />
-      </form>
-      <Empty height="2rem" />
-      <ButtonLogIn />
+        <Empty height="2rem" />
+        <Button>로그인 하기</Button>
+      </Form>
     </>
   );
 }
+
+export const InputPW = () => (
+  <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <Typography.Text style={{ fontSize: '2rem' }}>PW</Typography.Text>
+    <Input type="password" />
+  </div>
+);
+
+export const InputID = () => (
+  <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <Typography.Text style={{ fontSize: '2rem' }}>ID</Typography.Text>
+    <Input />
+  </div>
+);
--- src/Login/Login.styled.tsx
+++ src/Login/Login.styled.tsx
@@ -1,9 +1,9 @@
-import { Input, Typography, Button } from 'antd';
+import { Input as aInput, Button as aButton } from 'antd';
 import styled from '@emotion/styled';
+
 // Types
 type EmptyType = { height: string };
 
-// [Todo] 좀 더 관리 이쁘게 안되나
 export const Layout = styled.div({
   display: 'flex',
   justifyContent: 'center',
@@ -15,45 +15,23 @@ export const Empty = styled('div')<EmptyType>`
   height: ${props => `${props.height}`};
 `;
 
-export const InputID = () => (
-  <>
-    <Typography.Text style={{ fontSize: '2rem' }}>ID</Typography.Text>
-    <Input
-      style={{
-        backgroundColor: '#f4f4f4',
-        height: '3rem',
-        width: '20rem',
-        borderRadius: '15px',
-      }}
-    />
-  </>
-);
+export const Input = styled(aInput)`
+  background-color: #f4f4f4;
+  height: 3rem;
+  width: 20rem;
+  border-radius: 15px;
+`;
 
-export const InputPW = () => (
-  <>
-    <Typography.Text style={{ fontSize: '2rem' }}>PW</Typography.Text>
-    <Input
-      style={{
-        backgroundColor: '#f4f4f4',
-        height: '3rem',
-        width: '20rem',
-        borderRadius: '15px',
-      }}
-      type="password"
-    />
-  </>
-);
+// [Todo] 로그인 하기 font 설정하기
+export const Button = styled(aButton)`
+  background-color: #f4f4f4;
+  height: 3rem;
+  width: 12rem;
+  border-radius: 15px;
+`;
 
-export const ButtonLogIn = () => (
-  <Button
-    style={{
-      backgroundColor: '#f4f4f4',
-      height: '3rem',
-      width: '12rem',
-      borderRadius: '15px',
-    }}
-    form="sign-in"
-  >
-    로그인 하기
-  </Button>
-);
+export const Form = styled('form')`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
