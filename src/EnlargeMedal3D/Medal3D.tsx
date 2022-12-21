fccacc30033d6dfc6c58c8bdc84f302575394b9a
Jchan <gkswocks123@gmail.com>
2022-12-21 21:33:26 +0900

    Feat : Add models

--- src/EnlargeMedal3D/Medal3D.tsx
+++ src/EnlargeMedal3D/Medal3D.tsx
@@ -3,7 +3,9 @@ import { useGLTF } from '@react-three/drei';
 
 function Medal3D() {
   // const gltf: any = useGLTF('../assets/3d/medalA.gltf');
-  const gltf: any = useGLTF('assets/3d/medalA.gltf');
+  const gltf: any = useGLTF(
+    'https://cdn.jsdelivr.net/gh/Sean-Bradley/React-Three-Fiber-Boilerplate@useGLTF/public/models/tapeMeasure.glb',
+  );
   return <primitive object={gltf.scene} />;
 }
 
--- src/EnlargeMedal3D/index.tsx
+++ src/EnlargeMedal3D/index.tsx
@@ -2,6 +2,8 @@ import { Canvas } from '@react-three/fiber';
 import Medal3D from './Medal3D';
 
 export function EnlargeMedal3D() {
+  let scene = new THREE.Scene();
+
   return (
     <Canvas>
       <Medal3D />
