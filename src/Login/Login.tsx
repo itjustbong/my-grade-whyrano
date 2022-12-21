79d414699795e54ff84046cb2cd6d0efb34bd9fa
Jchan <gkswocks123@gmail.com>
2022-12-21 18:15:53 +0900

    Feat : 유효성 검사 구현

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { LoginBox } from './Login.component';
 import { Layout, Empty } from './Login.styled';
 import { ReactComponent as MainLogo } from '../assets/main_logo.svg';
-import { postSignIn } from './api/sign';
+import { postSignIn } from '../api/sign';
 
 const Login = () => {
-  // const onLogIn = () => {};
-
+  const onLogin = (id: string, pw: string) => {
+    postSignIn({ id, pw })
+      .then(res => res)
+      .catch(err => err);
+  };
   return (
     <Layout>
       <Empty height="6rem" />
       {/* [Todo] 메인 로고 사이즈 가변으로 ㄱㄱ */}
       <MainLogo />
       <Empty height="6rem" />
-      <LoginBox />
+      <LoginBox onLogin={onLogin} />
     </Layout>
   );
 };
